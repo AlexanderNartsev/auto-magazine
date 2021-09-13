0569ddff21196abbbf01672c4f13eeb9a3e85ad9
Alexander Nartsev <sainte-man@yandex.ru>
2021-09-13 23:05:19 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# easy-02
-auto-magazine
+# auto-magazine
+
+Страница со станьями об автомобилях
+
+## Ссылка на макет (easy-02)
+* https://www.figma.com/file/G3UWFlQmNtNs67751YiDH2/Month-of-Landings?node-id=2%3A1369
+
+## Ссылка на проект
+* https://alexandernartsev.github.io/auto-magazine/
+
+## Ссылка на репозиторий
+* https://github.com/AlexanderNartsev/auto-magazine
---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const page = document.querySelector('.page');
+const header = page.querySelector('.header');
+const menu = header.querySelector('.header__menu');
+const buttonMenu = header.querySelector('.button_type_burger');
+const buttonClose = header.querySelector('.button_type_close');
+
+function openMobileMenu() {
+
+    header.classList.add('header_mobile');
+    menu.classList.add('header__menu_mobile');
+    buttonClose.classList.remove('button_hide');
+    buttonMenu.classList.add('button_hide');
+
+}
+
+function closeMobileMenu() {
+
+    header.classList.remove('header_mobile');
+    menu.classList.remove('header__menu_mobile');
+    buttonClose.classList.add('button_hide');
+    buttonMenu.classList.remove('button_hide');
+    
+}
+
+buttonMenu.addEventListener('click', openMobileMenu);
+buttonClose.addEventListener('click', closeMobileMenu);
